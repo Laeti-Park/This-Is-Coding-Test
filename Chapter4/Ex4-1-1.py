0cbb58cde4fa336fc8684191c6940e2df7002507
Laeti-Park <creator98@naver.com>
2022-11-24 00:41:43 +0900

    Add Ex4-1-1

---
+++ Chapter4/Ex4-1-1.py
@@ -0,0 +1,19 @@
+N = int(input())
+x, y = 1, 1
+root = str(input()).split()
+
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+move_types = ['L', 'R', 'U', 'D']
+
+for i in root:
+    for j in range(len(move_types)):
+        if i == move_types[j]:
+            nx = x + dx[j]
+            ny = y + dy[j]
+    
+    if nx < 1 or ny < 1 or nx > N or ny > N:
+        continue
+    x, y = nx, ny
+
+print(x, y)
